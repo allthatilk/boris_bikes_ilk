e853742bc8c2611e7a873016530f21a599cd8a86
evebalog <evebalog@netscape.net>
2017-02-16 16:56:11 +0000

    Unit 11 update

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,9 @@ require_relative 'bike'
 
 class DockingStation
 
+  def dock
+  end
+
   def release_bike
     Bike.new
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,11 @@ require 'docking_station'
 describe DockingStation do
 it { is_expected.to respond_to :release_bike}
 
+it 'docks a bike' do
+  bike = subject.dock
+  # expect(bike).to eq 
+end
+
 it 'gets a bike' do
   bike = subject.release_bike
   expect(bike).to be_working
