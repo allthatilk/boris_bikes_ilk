94c569853cd92e6874c3056169d5322e5fd4ec36
evebalog <evebalog@netscape.net>
2017-02-16 14:25:07 +0000

    Docking Station released bike, expected it to be working, passed tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
-#bike = DockingStation.new
 it { is_expected.to respond_to :release_bike}
+
+it 'gets a bike' do
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
+
+
 end
